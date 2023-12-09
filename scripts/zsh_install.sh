e861986c4d647ec6015b34b73c38f83c6d8073d5
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2023-12-09 14:07:46 +0100

    Fix paths and setting default shell

--- scripts/zsh_install.sh
+++ scripts/zsh_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y zsh
+if ! command -v zsh &> /dev/null; then
+  sudo apt-get update && sudo apt-get install -y zsh
+fi
 
 mkdir -p ~/.antigen
 curl -L git.io/antigen > ~/.antigen/antigen.zsh
-cp ../files/.zshrc ~/.zshrc
-cp ../files/.p10k.zsh ~/.p10k.zsh
-chsh -s $(which zsh)
+cp ~/.dotfiles/files/.zshrc ~/.zshrc
+cp ~/.dotfiles/files/.p10k.zsh ~/.p10k.zsh
+sudo chsh -s $(which zsh) $(whoami)
